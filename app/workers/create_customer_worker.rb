c6abab15eb1885a3bd7c0c840d9d1e5bf110373b
Rafael George <george.rafael@gmail.com>
2014-02-18 17:51:05 -0400

    Refactor: CreateCustomerWorker extract a new create_customer method

--- app/workers/create_customer_worker.rb
+++ app/workers/create_customer_worker.rb
@@ -2,18 +2,22 @@ class CreateCustomerWorker
   include Sidekiq::Worker
 
   def perform(user_id, stripe_token, coupon)
-    user = User.find(user_id)
-    create_attrs = {email: user.email,
-                    description: user.name, 
+    @user = User.find(user_id)
+    create_customer(stripe_token, coupon)
+    @user.last_4_digits = @customer.cards.data.first["last4"]
+    @user.customer_id = @customer.id
+    @user.stripe_token = nil
+    @user.save!
+  end
+
+  def create_customer(stripe_token, coupon)
+    create_attrs = {email: @user.email,
+                    description: @user.name, 
                     card: stripe_token, 
-                    plan: user.roles.first.name}
+                    plan: @user.roles.first.name}
     unless coupon.blank?
       create_attrs.merge! coupon:coupon
     end
-    customer = Stripe::Customer.create(create_attrs)
-    user.last_4_digits = customer.cards.data.first["last4"]
-    user.customer_id = customer.id
-    user.stripe_token = nil
-    user.save!
+    @customer = Stripe::Customer.create(create_attrs)
   end
 end
